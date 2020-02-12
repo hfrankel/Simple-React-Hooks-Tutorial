bf6f00ae51ab51697e5cddd998e90883ecf33cf0
Harry Frankel <hfranks92@gmail.com>
2020-02-13 10:09:32 +1100

    Tutorial

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async () => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+    useEffect(
+        () => {
+            (async resource => {
+                const response = await axios.get(
+                    `https://jsonplaceholder.typicode.com/${resource}`
+            );
 
-        this.setState({ resources: response.data });
-    }
+                setResources(response.data);
+            })(resource);
+        },
+        [resource]
+    );
+
 
     return(
         <div>{resources.length}</div>
