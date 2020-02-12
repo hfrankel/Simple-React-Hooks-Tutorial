2f777052020ab6b908c7bf01a1f3964ce00c453a
Harry Frankel <hfranks92@gmail.com>
2020-02-13 10:29:17 +1100

    Tutorial

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
     const [resources, setResources] = useState([]);
 
     useEffect(
@@ -17,9 +17,16 @@ const ResourceList = ({ resource }) => {
         [resource]
     );
 
+    return resources;
+};
+
+const ResourceList = ({ resource }) => {
+    const resources = useResources(resource);
 
     return(
-        <div>{resources.length}</div>
+        <ul>
+            {resources.map(record => <li key={record.id}>{record.title}</li>)}
+        </ul>
     );
 }
 
